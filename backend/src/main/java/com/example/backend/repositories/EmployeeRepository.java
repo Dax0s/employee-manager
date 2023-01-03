2ddb1bd26b1556773b0cef35a47665e4d0ed715c
Domas K <klimaviciusdomas@gmail.com>
2023-01-03 13:11:58 +0200

    Created files for employee controller, service and repository

---
+++ backend/src/main/java/com/example/backend/repositories/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.backend.repositories;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface EmployeeRepository {
+}
